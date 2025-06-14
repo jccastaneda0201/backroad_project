cee24deac0e648b44268234967dc2ca623dad1cc
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-06-14 17:47:45 +0200

    update README

--- src/App.jsx
+++ src/App.jsx
@@ -2,77 +2,77 @@
 const App = () => {
   return (
     <>
-      <nav class="navbar">
-        <div class="nav-center">
-          <div class="nav-header">
-            <img src="./images/logo.svg" class="nav-logo" alt="backroads" />
-            <button type="button" class="nav-toggle" id="nav-toggle">
-              <i class="fas fa-bars"></i>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
             </button>
           </div>
-          <ul class="nav-links" id="nav-links">
+          <ul className="nav-links" id="nav-links">
             <li>
-              <a href="#home" class="nav-link"> home </a>
+              <a href="#home" className="nav-link"> home </a>
             </li>
 
             <li>
-              <a href="#about" class="nav-link"> about </a>
+              <a href="#about" className="nav-link"> about </a>
             </li>
 
             <li>
-              <a href="#services" class="nav-link"> services </a>
+              <a href="#services" className="nav-link"> services </a>
             </li>
 
             <li>
-              <a href="#tours" class="nav-link"> tours</a>
+              <a href="#tours" className="nav-link"> tours</a>
             </li>
           </ul>
 
-          <ul class="nav-icons">
+          <ul className="nav-icons">
             <li>
-              <a href="https://www.twitter.com" target="_blank" class="nav-icon"
-              ><i class="fab fa-facebook"></i
+              <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-facebook"></i
               ></a>
             </li>
             <li>
-              <a href="https://www.twitter.com" target="_blank" class="nav-icon"
-              ><i class="fab fa-twitter"></i
+              <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-twitter"></i
               ></a>
             </li>
             <li>
-              <a href="https://www.twitter.com" target="_blank" class="nav-icon"
-              ><i class="fab fa-squarespace"></i
+              <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-squarespace"></i
               ></a>
             </li>
           </ul>
         </div>
       </nav>
 
-      <section class="hero" id="home">
-        <div class="hero-banner">
+      <section className="hero" id="home">
+        <div className="hero-banner">
           <h1>continue exploring</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
             explicabo debitis est autem dicta.
           </p>
-          <a href="#tours" class="btn hero-btn">explore tours</a>
+          <a href="#tours" className="btn hero-btn">explore tours</a>
         </div>
       </section>
 
-      <section class="section" id="about">
-        <div class="section-title">
+      <section className="section" id="about">
+        <div className="section-title">
           <h2>about <span>us</span></h2>
         </div>
 
-        <div class="section-center about-center">
-          <div class="about-img">
+        <div className="section-center about-center">
+          <div className="about-img">
             <img
               src="./images/about.jpeg"
-              class="about-photo"
+              className="about-photo"
               alt="awesome beach"
             />
           </div>
-          <article class="about-info">
+          <article className="about-info">
             <h3>explore the difference</h3>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
@@ -84,43 +84,43 @@ const App = () => {
               quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
               unde dolor?
             </p>
-            <a href="#" class="btn">read more</a>
+            <a href="#" className="btn">read more</a>
           </article>
         </div>
       </section>
 
-      <section class="section services" id="services">
-        <div class="section-title">
+      <section className="section services" id="services">
+        <div className="section-title">
           <h2>our <span>services</span></h2>
         </div>
-        <div class="section-center services-center">
-          <article class="service">
-            <span class="service-icon"><i class="fas fa-wallet fa-fw"></i></span>
-            <div class="service-info">
-              <h4 class="service-title">saving money</h4>
-              <p class="service-text">
+        <div className="section-center services-center">
+          <article className="service">
+            <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
+            <div className="service-info">
+              <h4 className="service-title">saving money</h4>
+              <p className="service-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores, officia.
               </p>
             </div>
           </article>
 
-          <article class="service">
-            <span class="service-icon"><i class="fas fa-tree fa-fw"></i></span>
-            <div class="service-info">
-              <h4 class="service-title">endless hiking</h4>
-              <p class="service-text">
+          <article className="service">
+            <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
+            <div className="service-info">
+              <h4 className="service-title">endless hiking</h4>
+              <p className="service-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores, officia.
               </p>
             </div>
           </article>
 
-          <article class="service">
-            <span class="service-icon"><i class="fas fa-socks fa-fw"></i></span>
-            <div class="service-info">
-              <h4 class="service-title">amazing comfort</h4>
-              <p class="service-text">
+          <article className="service">
+            <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
+            <div className="service-info">
+              <h4 className="service-title">amazing comfort</h4>
+              <p className="service-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores, officia.
               </p>
@@ -129,19 +129,19 @@ const App = () => {
         </div>
       </section>
 
-      <section class="section" id="tours">
-        <div class="section-title">
+      <section className="section" id="tours">
+        <div className="section-title">
           <h2>featured <span>tours</span></h2>
         </div>
 
-        <div class="section-center featured-center">
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-1.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">august 26th, 2020</p>
+        <div className="section-center featured-center">
+          <article className="tour-card">
+            <div className="tour-img-container">
+              <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
+              <p className="tour-date">august 26th, 2020</p>
             </div>
-            <div class="tour-info">
-              <div class="tour-title">
+            <div className="tour-info">
+              <div className="tour-title">
                 <h4>Tibet Adventure</h4>
               </div>
               <p>
@@ -149,9 +149,9 @@ const App = () => {
                 vitae tempore voluptatum maxime reprehenderit eum quod
                 exercitationem fugit, qui corporis.
               </p>
-              <div class="tour-footer">
+              <div className="tour-footer">
                 <p>
-                  <span><i class="fas fa-map"></i></span> china
+                  <span><i className="fas fa-map"></i></span> china
                 </p>
                 <p>6 days</p>
                 <p>from $2100</p>
@@ -159,21 +159,21 @@ const App = () => {
             </div>
           </article>
 
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-2.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">october 1th, 2020</p>
+          <article className="tour-card">
+            <div className="tour-img-container">
+              <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
+              <p className="tour-date">october 1th, 2020</p>
             </div>
-            <div class="tour-info">
+            <div className="tour-info">
               <h4>best of java</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
                 vitae tempore voluptatum maxime reprehenderit eum quod
                 exercitationem fugit, qui corporis.
               </p>
-              <div class="tour-footer">
+              <div className="tour-footer">
                 <p>
-                  <span><i class="fas fa-map"></i></span> indonesia
+                  <span><i className="fas fa-map"></i></span> indonesia
                 </p>
                 <p>11 days</p>
                 <p>from $1400</p>
@@ -181,21 +181,21 @@ const App = () => {
             </div>
           </article>
 
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-3.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">september 15th, 2020</p>
+          <article className="tour-card">
+            <div className="tour-img-container">
+              <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
+              <p className="tour-date">september 15th, 2020</p>
             </div>
-            <div class="tour-info">
+            <div className="tour-info">
               <h4>explore hong kong</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
                 vitae tempore voluptatum maxime reprehenderit eum quod
                 exercitationem fugit, qui corporis.
               </p>
-              <div class="tour-footer">
+              <div className="tour-footer">
                 <p>
-                  <span><i class="fas fa-map"></i></span> hong kong
+                  <span><i className="fas fa-map"></i></span> hong kong
                 </p>
                 <p>8 days</p>
                 <p>from $5000</p>
@@ -203,21 +203,21 @@ const App = () => {
             </div>
           </article>
 
-          <article class="tour-card">
-            <div class="tour-img-container">
-              <img src="./images/tour-4.jpeg" class="tour-img" alt="" />
-              <p class="tour-date">december 5th, 2019</p>
+          <article className="tour-card">
+            <div className="tour-img-container">
+              <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
+              <p className="tour-date">december 5th, 2019</p>
             </div>
-            <div class="tour-info">
+            <div className="tour-info">
               <h4>kenya highlights</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
                 vitae tempore voluptatum maxime reprehenderit eum quod
                 exercitationem fugit, qui corporis.
               </p>
-              <div class="tour-footer">
+              <div className="tour-footer">
                 <p>
-                  <span><i class="fas fa-map"></i></span> kenya
+                  <span><i className="fas fa-map"></i></span> kenya
                 </p>
                 <p>20 days</p>
                 <p>from $3300</p>
@@ -227,39 +227,39 @@ const App = () => {
         </div>
       </section>
 
-      <footer class="section footer">
-        <ul class="footer-links">
+      <footer className="section footer">
+        <ul className="footer-links">
           <li>
-            <a href="#home" class="footer-link">home</a>
+            <a href="#home" className="footer-link">home</a>
           </li>
           <li>
-            <a href="#about" class="footer-link">about</a>
+            <a href="#about" className="footer-link">about</a>
           </li>
           <li>
-            <a href="#services" class="footer-link">services</a>
+            <a href="#services" className="footer-link">services</a>
           </li>
           <li>
-            <a href="#featured" class="footer-link">featured</a>
-          </li>
-        </ul>
-        <ul class="footer-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="footer-icon"
-            ><i class="fab fa-facebook"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="footer-icon"
-            ><i class="fab fa-twitter"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="footer-icon"
-            ><i class="fab fa-squarespace"></i
-            ></a>
+            <a href="#featured" className="footer-link">featured</a>
           </li>
         </ul>
-        <p class="copyright">
+        <ul className="footer-icons">
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="footer-icon"
+            ><i className="fab fa-facebook"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="footer-icon"
+            ><i className="fab fa-twitter"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="footer-icon"
+            ><i className="fab fa-squarespace"></i
+            ></a>
+          </li>
+        </ul>
+        <p className="copyright">
           copyright &copy; Backroads travel tours company
           <span id="date"></span> all rights reserved
         </p>
